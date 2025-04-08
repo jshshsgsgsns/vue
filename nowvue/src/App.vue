ba729d85d1413193b9ccf8f9f72bd91f8174b13a
jshshsgsgsns <2217795051@qq.com>
2025-04-08 21:18:27 +0800

    1

---
+++ nowvue/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+import son from './components/HelloWorld.vue'
+
+let num = ref(1)
+
+const add = () => {
+  num.value += 1; // 正确修改响应式数据
+}
+</script>
+
+<template>
+  <p @click="add">{{ num }}</p> <!-- 使用 Vue 的事件绑定方式 -->
+  <son msg = "123" num = '14'></son>
+</template>
+
+<style scoped>
+</style>
---
+++ nowvue/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+  num: Number
+})
+
+
+</script>
+  
+<template>
+    <h3>{{ msg }}和{{ num }}</h3>
+</template>
+
+<style scoped>
+
+</style>
